1cc2f892a3a63c976a7b46ef9f5c5a4c1ae113e5
Chris Meyers <cm.02.93@gmail.com>
2016-11-12 17:32:40 -0500

    converted tabs to spaces.

--- waitForWritable.py
+++ waitForWritable.py
@@ -5,31 +5,31 @@ import sys
 import time
 
 def wait_until_writable():
-	print("Waiting for " + file_name + " to become writable ", end="", flush=True)
-	while not is_writable():
-		print(". ", end="", flush=True)
-		try:
-			time.sleep(1)
-		except KeyboardInterrupt:
-			sys.exit(2)
+    print("Waiting for " + file_name + " to become writable ", end="", flush=True)
+    while not is_writable():
+        print(". ", end="", flush=True)
+        try:
+            time.sleep(1)
+        except KeyboardInterrupt:
+            sys.exit(2)
 
-	print(file_name + " is now writable!")
+    print(file_name + " is now writable!")
 
 
 def is_writable():
-	return os.access(file_name, os.W_OK)
+    return os.access(file_name, os.W_OK)
 
 
 def display_usage():
-	print("Supply a read-only file.")
+    print("Supply a read-only file.")
 
 
 if __name__ == '__main__':
-	if len(sys.argv) >= 2:
-		file_name = sys.argv[1]
-		if not is_writable():
-			wait_until_writable()
-		else:
-			display_usage()
-	else:
-		display_usage()
+    if len(sys.argv) >= 2:
+        file_name = sys.argv[1]
+        if not is_writable():
+            wait_until_writable()
+        else:
+            display_usage()
+    else:
+        display_usage()
